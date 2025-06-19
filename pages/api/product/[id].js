dc9695bced3bd01bba5dcd79acb2a003e478cf19
Tasleem <tasleembca31@gmail.com>
2025-06-19 11:39:06 +0530

    added seperate products

---
+++ pages/api/product/[id].js
@@ -0,0 +1,36 @@
+import connectDB from '../../../utils/connectDB';
+import Product from '../../../models/productModel';
+import { HttpStatus, USER_SIGNIN, SUCCESS, ERROR } from "../constants";
+
+connectDB();
+
+export default async (req, res) => {
+  switch (req.method) {
+    case "GET":
+      await getProduct(req, res);
+      break;
+
+    default:
+      break;
+  }
+}
+
+const getProduct = async (req, res) => {
+  try {
+    const { id } = req.query;
+    const product = await Product.findById(id);
+    console.log(product);
+
+    if (!product) {
+      return res.status(HttpStatus['NotFound']).json({ message: "Product not found." });
+    }
+
+    return res.status(HttpStatus['OK']).json({
+      status: "Products retrieved successfully!",
+      product
+    });
+
+  } catch (error) {
+    return res.status(HttpStatus['InternalServerError']).json({ message: error.message });
+  }
+};
--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,7 +1,71 @@
-import React from 'react'
+import Head from 'next/head'
+import React, { useState } from 'react'
+import { getData } from '../../utils/fetchData';
 
-export default function ProductDetails() {
+export default function ProductDetails({ product }) {
+  console.log(product);
+  const [productItem, setProductItem] = useState(product);
+  const [productIndex, setProductIndex] = useState(0);
+
   return (
-    <div>ProductDetails</div>
+    <div className='product_details'>
+      <div className='row detail_page mt-4'>
+        <Head><title>Product Details Page</title></Head>
+        <div className='col-md-6'>
+          <img src={productItem.images[productIndex].url} alt={productItem.title}
+            className='d-block rounded img-thumbnail w-100' style={{ height: '350px' }} />
+
+          <div className='row mx-0 mt-2 small-img'>
+            {
+              productItem.images.map((img, index) => (
+                <img
+                  key={img.url}
+                  src={img.url}
+                  alt={productItem.title}
+                  style={{
+                    height: '80px',
+                    width: '20%',
+                    cursor: 'pointer',
+                    border: productIndex === index ? '4px solid #dc3545' : undefined
+                  }}
+                  onClick={() => setProductIndex(index)}
+                  className="img-thumbnail rounded mr-1"
+                />
+              ))
+            }
+          </div>
+        </div>
+        <div className='col-md-6'>
+          <h2 className='text-uppercase text-secondary'>{productItem.title}</h2>
+          <h5 className='text-danger'>${productItem.price}</h5>
+          <p>{productItem.description}</p>
+          <div className='row justify-content-between align-items-center mx-0'>
+            <p className={`${productItem.inStock > 0 ? 'text-success' : 'text-danger'}`}>In Stock: {productItem.inStock}</p>
+            <p className='text-danger'>Sold: {productItem.sold}</p>
+          </div>
+          <div className='row justify-content-between'>
+            <button className='btn btn-dark mr-2' style={{ width: '48%' }}>
+              <i className="fas fa-cart-plus mr-2"></i>
+              Add to Cart
+            </button>
+            <button className='btn btn-dark' style={{ width: '48%' }}>
+              <i className="fas fa-shopping-cart mr-1"></i>
+              Buy
+            </button>
+          </div>
+        </div>
+      </div>
+    </div >
   )
+}
+
+export async function getServerSideProps({ params: { id } }) {
+
+  const res = await getData(`product/${id}`);
+  console.log(res);
+  return {
+    props: {
+      product: res.product,
+    },
+  };
 }
